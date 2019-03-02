d36100d98c0a2b698c3b2b881874c198cf4de69b
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-02 17:07:18 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,5 @@
-# Write your code here.
+def batch_badge_creator(name)
+  name = ["Edsger, Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+batch_badge_creator do |name|
+  puts "Hello, my name is #{name}."
+end
