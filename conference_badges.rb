3939a03a4812e913e044554a84beefcd8296fa1e
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-02 21:15:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,25 @@
-def batch_badge_creator(name)
-  name = ["Edsger, Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-batch_badge_creator do |name|
-  puts "Hello, my name is #{name}."
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+  
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    badge_maker(attendee)
+  end
+end
+
+def assign_rooms(attendees)
+  array = []
+  attendees.each_with_index do |attendee, index|
+    array.push("Hello, #{attendee}! You'll be assigned to room #{index += 1}!")
+  end
+  array
+end
+
+def printer(attendees)
+  badges_and_room_assignments.each do |attendee|
+  batch_badge_creator(attendee)
+  assign_rooms(attendee)
+  end
+  printer(attendees)
 end
